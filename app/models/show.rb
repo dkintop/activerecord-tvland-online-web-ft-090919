d4b6bb45c8bd135acd1b2e59d581584247cb58f1
Doug Kintop <dkintop@hotmail.com>
2019-10-16 21:53:48 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
-
+  belongs_to :network
   def actors_list
   
   self.actors.collect{|actor| actor.name}
