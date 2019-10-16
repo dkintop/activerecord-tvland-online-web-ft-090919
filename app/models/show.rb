ba5a6ea5b5c4890f6bca91cc5d036ed02ffb991c
Doug Kintop <dkintop@hotmail.com>
2019-10-16 21:51:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
+require 'pry'
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+  self.characters.collect
+  binding.pry
+  end 
+
 end
